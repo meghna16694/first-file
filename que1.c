bfe8de183775e484af1d2968895a3b946c15f52f
meghna16694 <meghnagondaliya15@gmail.com>
2023-11-29 16:36:39 +0530

    message

--- que1.c
+++ que1.c
@@ -1,5 +1,17 @@
 #include<stdio.h>
 
 int main(){
-    printf("Hello World");
+    
+    int a,b;
+
+    printf("Enter value of first number\n");
+    scanf("%d",&a);
+
+    
+    printf("Enter value of second number\n");
+    scanf("%d",&b);
+
+    if (a>b){
+        printf("a is greater than b%d",a);
+    }
 }
