107541d762a30996f0190f3ba4910100e564bdda
meghna16694 <meghnagondaliya15@gmail.com>
2023-12-06 15:10:56 +0530

    task11

---
+++ Day-11/que3.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main(){
+
+    int a,i;
+
+    printf("Enter number: ");
+    scanf("%d",&a);
+
+    i = 1;
+
+    do{
+        if (i % 2 == 0);
+        printf("%d\n",i);
+        i++;
+    } while(i <= a);
+}
---
+++ Day-11/que6.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main(){
+    
+  int a;
+  
+  int sum = 0;
+  
+  printf("Enter any number: ");
+  scanf("%d",& a);
+  
+  for( int i=1; i<=a; i++){
+      sum +=i;
+      
+      printf("%d\n", sum);
+  }
+
+}
